81b97937dbbe10c57856056948c47a58fcb6a36a
Lance Pollard <lancejpollard@gmail.com>
2010-06-04 00:22:24 -0700

    small fix for rails 2 html_safe

--- lib/compressible/view_helpers.rb
+++ lib/compressible/view_helpers.rb
@@ -3,14 +3,25 @@ module Compressible::ViewHelpers
   def compressible_stylesheet_tag(*keys)
     Compressible.stylesheets_for(*keys).collect do |asset|
       stylesheet_link_tag(asset)
-    end.join("\n").html_safe
+    end.join("\n").send(safe_method)
   end
   
   def compressible_javascript_tag(*keys)
     Compressible.javascripts_for(*keys).collect do |asset|
       javascript_include_tag(asset)
-    end.join("\n").html_safe
+    end.join("\n").send(safe_method)
   end
+  
+  private
+    def safe_method
+      if "".respond_to?(:html_safe)
+        return :html_safe
+      elsif "".respond_to?(:html_safe!)
+        return :html_safe!
+      else
+        return :to_s
+      end
+    end
   
 end
 
